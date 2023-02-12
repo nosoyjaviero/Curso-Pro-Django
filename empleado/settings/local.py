07bf817397776aa689a80bf1b7b86e6991f2f8cc
nosoyjaviero <lopez4935@gmail.com>
2023-02-12 04:28:37 -0600

    incluye los capitulos instalar una app, modificar los settings para que estos puedan ser abiertos en una carpeta de settings utilizando POO entre varios archivos. Incluye el capitulo de crear aplicaciones por aparte como si fueran un modulos (puede verse en empleadoBD.png) por que se necesita tenerlas por aparte

---
+++ empleado/settings/local.py
@@ -0,0 +1,35 @@
+from .base import *
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
