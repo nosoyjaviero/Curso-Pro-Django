0f5a68a0408d48240c4dacc5eecd0657ea9a350f
nosoyjaviero <lopez4935@gmail.com>
2023-02-14 01:55:51 -0600

    Cree el modelo Empleado y utilizamos Llaves foraneas

--- applications/personas/models.py
+++ applications/personas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from applications.departamento.models import Departamento
 
 # Create your models here.
+class Empleado(models.Model):
+    """Modelo para la tabla empleado
+
+    Args:
+        models (_type_): _description_
+    """
+    
+    JOB_CHOISES= {('1','Contador',),
+                  ('2','ADMINISTRADOR'),
+                  ('3','ECONOMISTA'),
+                  ('4','OTROS')}
+    
+    firts_name = models.CharField('Nombres', max_length=50)
+    last_name = models.CharField('Apellidos', max_length=50)
+    job = models.CharField('Trabajo', max_length=50,choices=JOB_CHOISES)
+    departamento_fk = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    # image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+    def __str__(self):
+     return str(self.id) +'- '+self.firts_name +'- '+self.last_name +'-'+self.job +'- '+str(self.departamento_fk)
