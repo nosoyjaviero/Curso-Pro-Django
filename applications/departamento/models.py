8e2867a1fd211ba34f7c2dbef81d2a74562e7fcd
nosoyjaviero <lopez4935@gmail.com>
2023-02-14 00:51:33 -0600

    Paramertros en campo de Modelo. Unique, Editable, Blank, Null

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Departamento(models.Model):
-    name =models.CharField('Nombre',max_length=50)
-    short_name= models.CharField('Nombre Corto', max_length=20)
+    name =models.CharField('Nombre',max_length=50, blank=True,editable=False )
+    short_name= models.CharField('Nombre Corto', max_length=20,unique=True)
     anulate=models.BooleanField('Anulado',default=False)
     
     def __str__(self):
-        return self.id +'-'+ self.name +'-'+  self.short_name
+        return str(self.id) +'-'+ self.name +'-'+  self.short_name
