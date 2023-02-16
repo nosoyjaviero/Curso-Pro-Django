1f8007ff2229da2da4a603d32d4c23bd80b3088b
nosoyjaviero <lopez4935@gmail.com>
2023-02-16 02:59:33 -0600

    Filtros Administrador de Django

--- applications/personas/admin.py
+++ applications/personas/admin.py
@@ -12,5 +12,15 @@ class EmpleadoAdmin(admin.ModelAdmin):
         'departamento_fk',
         'job',        
     )
+    #sirve para renderizar una barra de busqueda
+    search_fields=('first_name',)
+    
+    #a;adir filtros de busqueda 
+    list_filter=('job', 'habilidad', )
+    #solo sirve para relaciones muchos a muchos y sirve a;adir una barra de busqueda en la lista de habilidades que vamos a a;adir en caso de tener miles de habilidades por a;adir.
+    filter_horizontal = ('habilidad',)
+    #sirve para realizar un filtro de busqueda
+    
+    #esta linea y class EmpleadoAdmin() se fucionan y muestran una tabla en http://127.0.0.1:8000/admin/personas/empleado/
     
 admin.site.register(Empleado, EmpleadoAdmin)
