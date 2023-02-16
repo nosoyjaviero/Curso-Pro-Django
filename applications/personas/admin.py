aef8c34011e670066c2febacfd2efce6febe0e42
nosoyjaviero <lopez4935@gmail.com>
2023-02-16 03:53:00 -0600

    Funciones especiales en el Admin. Inventar un campo en la tabla del administrador de django

--- applications/personas/admin.py
+++ applications/personas/admin.py
@@ -7,10 +7,14 @@ admin.site.register(Habilidades)
 #Heredamos admin.ModelAdmin
 class EmpleadoAdmin(admin.ModelAdmin):
     list_display=(
+        #solo puedo llamar a los atributos que existen en mi modelo
         'firts_name',
         'last_name',
         'departamento_fk',
-        'job',        
+        'job',   
+        #si necesitara inventarme una columna en django debo hacer:
+        #full name no existe en modelos sera columna que nos inventamos con una funcion        
+         'full_name'  
     )
     #sirve para renderizar una barra de busqueda
     search_fields=('first_name',)
@@ -21,6 +25,15 @@ class EmpleadoAdmin(admin.ModelAdmin):
     filter_horizontal = ('habilidad',)
     #sirve para realizar un filtro de busqueda
     
-    #esta linea y class EmpleadoAdmin() se fucionan y muestran una tabla en http://127.0.0.1:8000/admin/personas/empleado/
+    #nos inventaremos la columna menciona arriba con esta funcion     
     
+    def full_name(self,obj):
+        # print(obj)
+        # print(obj.firts_name)
+        # print(obj.last_name)
+        # print(obj.job)
+        return f'{obj.firts_name} {obj.last_name}'
+    
+    #esta linea y class EmpleadoAdmin() se fucionan y muestran una tabla en http://127.0.0.1:8000/admin/personas/empleado/
+        
 admin.site.register(Empleado, EmpleadoAdmin)
